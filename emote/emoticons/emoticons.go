12ef4c9d3f9b20b77f57f3cdce3b2226eb666585
Chip Hudgins <mudzi42@gmail.com>
2019-09-17 08:33:42 -0700

    added emotecli

---
+++ emote/emoticons/emoticons.go
@@ -0,0 +1,41 @@
+package emoticons
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/atotto/clipboard"
+	"github.com/mudzi42/playground/emote/config"
+)
+
+type App struct {
+	Out    io.Writer
+	Config *config.Config
+}
+
+func New() (*App, error) {
+
+	c, err := config.Load()
+	if err != nil {
+		return nil, err
+	}
+
+	a := &App{
+		Out:    os.Stdout,
+		Config: c,
+	}
+	return a, nil
+}
+
+func (a *App) Emote(name string, dest string) {
+	emoticon := a.Config.Emoticon[name]
+
+	switch dest {
+	case "clipboard":
+		clipboard.WriteAll(emoticon)
+		fmt.Fprintf(a.Out, emoticon, "was copied to the clipboard")
+	default:
+		fmt.Fprintf(a.Out, emoticon)
+	}
+}
