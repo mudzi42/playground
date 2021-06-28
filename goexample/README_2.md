4c2250115d88134b3249207cdcb248ee76f3f03f
Chip Hudgins <mudzi42@gmail.com>
2021-06-28 09:54:52 -0700

    string functions

--- goexample/README_2.md
+++ goexample/README_2.md
@@ -3,3 +3,4 @@
 3. https://gobyexample.com/panic
 4. https://gobyexample.com/defer
 5. https://gobyexample.com/collection-functions
+6. https://gobyexample.com/string-functions
--- goexample/main.go
+++ goexample/main.go
@@ -2,76 +2,27 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	s "strings"
 )
 
-func Index(vs []string, t string) int {
-	for i, v := range vs {
-		if v == t {
-			return i
-		}
-	}
-	return -1
-}
+var p = fmt.Println
 
-func Include(vs []string, t string) bool {
-	return Index(vs, t) >= 0
-}
-
-func Any(vs []string, f func(string) bool) bool {
-	for _, v := range vs {
-		if f(v) {
-			return true
-		}
-	}
-	return false
-}
-
-func All(vs []string, f func(string) bool) bool {
-	for _, v := range vs {
-		if !f(v) {
-			return false
-		}
-	}
-	return true
-}
-
-func Filter(vs []string, f func(string) bool) []string {
-	vsf := make([]string, 0)
-	for _, v := range vs {
-		if f(v) {
-			vsf = append(vsf, v)
-		}
-	}
-	return vsf
-}
-
-func Map(vs []string, f func(string) string) []string {
-	vsm := make([]string, len(vs))
-	for i, v := range vs {
-		vsm[i] = f(v)
-	}
-	return vsm
-}
 func main() {
-	var strs = []string{"peach", "apple", "pear", "plum"}
-	fmt.Println(Index(strs, "pear"))
-	fmt.Println(Include(strs, "grape"))
 
-	fmt.Println(Any(strs, func(v string) bool {
-		return strings.HasPrefix(v, "p")
-	}))
+	p("Contains:  ", s.Contains("test", "es"))
+	p("Count:     ", s.Count("test", "t"))
+	p("HasPrefix: ", s.HasPrefix("test", "te"))
+	p("HasSuffix: ", s.HasSuffix("test", "st"))
+	p("Index:     ", s.Index("test", "e"))
+	p("Join:      ", s.Join([]string{"a", "b"}, "-"))
+	p("Repeat:    ", s.Repeat("a", 5))
+	p("Replace:   ", s.Replace("foo", "o", "0", -1))
+	p("Replace:   ", s.Replace("foo", "o", "0", 1))
+	p("Split:     ", s.Split("a-b-c-d-e", "-"))
+	p("ToLower:   ", s.ToLower("TEST"))
+	p("ToUpper:   ", s.ToUpper("test"))
+	p()
 
-	fmt.Println(All(strs, func(v string) bool {
-		return strings.HasPrefix(v, "p")
-	}))
-
-	fmt.Println(Filter(strs,
-		func(v string) bool {
-			return strings.Contains(v, "e")
-		},
-	))
-
-	fmt.Println(Map(strs, strings.ToUpper))
-
+	p("Len: ", len("hello"))
+	p("Char:", "hello"[1])
 }
