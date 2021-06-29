af48f9cc00fb84a7792dc8a8366136716241e6f6
Chip Hudgins <mudzi42@gmail.com>
2021-06-29 10:49:52 -0700

    string formatting

--- goexample/README_2.md
+++ goexample/README_2.md
@@ -4,3 +4,4 @@
 4. https://gobyexample.com/defer
 5. https://gobyexample.com/collection-functions
 6. https://gobyexample.com/string-functions
+7. https://gobyexample.com/string-formatting
--- goexample/main.go
+++ goexample/main.go
@@ -2,27 +2,59 @@ package main
 
 import (
 	"fmt"
-	s "strings"
+	"os"
 )
 
-var p = fmt.Println
+type point struct {
+	x, y int
+}
 
 func main() {
 
-	p("Contains:  ", s.Contains("test", "es"))
-	p("Count:     ", s.Count("test", "t"))
-	p("HasPrefix: ", s.HasPrefix("test", "te"))
-	p("HasSuffix: ", s.HasSuffix("test", "st"))
-	p("Index:     ", s.Index("test", "e"))
-	p("Join:      ", s.Join([]string{"a", "b"}, "-"))
-	p("Repeat:    ", s.Repeat("a", 5))
-	p("Replace:   ", s.Replace("foo", "o", "0", -1))
-	p("Replace:   ", s.Replace("foo", "o", "0", 1))
-	p("Split:     ", s.Split("a-b-c-d-e", "-"))
-	p("ToLower:   ", s.ToLower("TEST"))
-	p("ToUpper:   ", s.ToUpper("test"))
-	p()
+	p := point{1, 2}
+	fmt.Printf("%v\n", p)
 
-	p("Len: ", len("hello"))
-	p("Char:", "hello"[1])
+	fmt.Printf("%+v\n", p)
+
+	fmt.Printf("%#v\n", p)
+
+	fmt.Printf("%T\n", p)
+
+	fmt.Printf("%t\n", true)
+
+	fmt.Printf("%d\n", 123)
+
+	fmt.Printf("%b\n", 14)
+
+	fmt.Printf("%c\n", 33)
+
+	fmt.Printf("%x\n", 456)
+
+	fmt.Printf("%f\n", 78.9)
+
+	fmt.Printf("%e\n", 123400000.0)
+	fmt.Printf("%E\n", 123400000.0)
+
+	fmt.Printf("%s\n", "\"string\"")
+
+	fmt.Printf("%q\n", "string")
+
+	fmt.Printf("%x\n", "hex this")
+
+	fmt.Printf("%p\n", &p)
+
+	fmt.Printf("|%6d|%6d|\n", 12, 345)
+
+	fmt.Printf("|%6.2f|%6.2f|\n", 1.2, 3.45)
+
+	fmt.Printf("|%-6.2f|%-6.2f|\n", 1.2, 3.45)
+
+	fmt.Printf("|%6s|%6s|\n", "foo", "b")
+
+	fmt.Printf("|%-6s|%-6s|\n", "foo", "b")
+
+	s := fmt.Sprintf("a %s", "string")
+	fmt.Println(s)
+
+	fmt.Fprintf(os.Stderr, "an %s\n", "error")
 }
