cd7ad5b1d7dd59b750f3139511a1322124faeb8f
Chip Hudgins <mudzi42@gmail.com>
2021-07-09 16:24:13 -0700

    json

--- goexample/main.go
+++ goexample/main.go
@@ -1,43 +1,78 @@
 package main
 
 import (
-	"bytes"
+	"encoding/json"
 	"fmt"
-	"regexp"
+	"os"
 )
+
+type response1 struct {
+	Page   int
+	Fruits []string
+}
+
+type response2 struct {
+	Page   int      `json:"page"`
+	Fruits []string `json:"fruits"`
+}
 
 func main() {
 
-	match, _ := regexp.MatchString("p([a-z]+)ch", "peach")
-	fmt.Println(match)
+	bolB, _ := json.Marshal(true)
+	fmt.Println(string(bolB))
 
-	r, _ := regexp.Compile("p([a-z]+)ch")
+	intB, _ := json.Marshal(1)
+	fmt.Println(string(intB))
 
-	fmt.Println(r.MatchString("peach"))
+	fltB, _ := json.Marshal(2.34)
+	fmt.Println(string(fltB))
 
-	fmt.Println(r.FindString("peach punch"))
+	strB, _ := json.Marshal("gopher")
+	fmt.Println(string(strB))
 
-	fmt.Println(r.FindStringIndex("peach punch"))
+	slcD := []string{"apple", "peach", "pear"}
+	slcB, _ := json.Marshal(slcD)
+	fmt.Println(string(slcB))
 
-	fmt.Println(r.FindStringSubmatch("peach punch"))
+	mapD := map[string]int{"apple": 5, "lettuce": 7}
+	mapB, _ := json.Marshal(mapD)
+	fmt.Println(string(mapB))
 
-	fmt.Println(r.FindStringSubmatchIndex("peach punch"))
+	res1D := &response1{
+		Page:   1,
+		Fruits: []string{"apple", "peach", "pear"}}
+	res1B, _ := json.Marshal(res1D)
+	fmt.Println(string(res1B))
 
-	fmt.Println(r.FindAllString("peach punch pinch", -1))
+	res2D := &response2{
+		Page:   1,
+		Fruits: []string{"apple", "peach", "pear"}}
+	res2B, _ := json.Marshal(res2D)
+	fmt.Println(string(res2B))
 
-	fmt.Println(r.FindAllStringSubmatchIndex(
-		"peach punch pinch", -1))
+	byt := []byte(`{"num":6.13,"strs":["a","b"]}`)
 
-	fmt.Println(r.FindAllString("peach punch pinch", 2))
+	var dat map[string]interface{}
 
-	fmt.Println(r.Match([]byte("peach")))
+	if err := json.Unmarshal(byt, &dat); err != nil {
+		panic(err)
+	}
+	fmt.Println(dat)
 
-	r = regexp.MustCompile("p([a-z]+)ch")
-	fmt.Println(r)
+	num := dat["num"].(float64)
+	fmt.Println(num)
 
-	fmt.Println(r.ReplaceAllString("a peach", "<fruit>"))
+	strs := dat["strs"].([]interface{})
+	str1 := strs[0].(string)
+	fmt.Println(str1)
 
-	in := []byte("a peach")
-	out := r.ReplaceAllFunc(in, bytes.ToUpper)
-	fmt.Println(string(out))
+	str := `{"page": 1, "fruits": ["apple", "peach"]}`
+	res := response2{}
+	json.Unmarshal([]byte(str), &res)
+	fmt.Println(res)
+	fmt.Println(res.Fruits[0])
+
+	enc := json.NewEncoder(os.Stdout)
+	d := map[string]int{"apple": 5, "lettuce": 7}
+	enc.Encode(d)
 }
